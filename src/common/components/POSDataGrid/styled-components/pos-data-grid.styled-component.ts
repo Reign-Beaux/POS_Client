c607311eadf2820737d28d0a7357db7b6547c5ae
Reign-Beaux <samc920130@hotmail.com>
2023-05-20 16:01:06 -0600

    se hicieron modificaciones variadas

--- src/common/components/POSDataGrid/styled-components/pos-data-grid.styled-component.ts
+++ src/common/components/POSDataGrid/styled-components/pos-data-grid.styled-component.ts
@@ -2,6 +2,7 @@ import { styled } from "@mui/material/styles";
 import { DataGrid } from "@mui/x-data-grid";
 
 export const StyleDataGrid = styled(DataGrid)`
+  width: 100%;
   .MuiDataGrid-cell:focus-within {
     outline: none;
   }
