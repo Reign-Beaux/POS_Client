9de2001e3b231287578ba1df36833449900b2422
Reign-Beaux <samc920130@hotmail.com>
2023-05-27 14:37:46 -0600

    se creo el header

---
+++ src/common/components/POSDialogHeader/POSDialogHeader.tsx
@@ -0,0 +1,31 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { AppBar, Toolbar, IconButton, Typography, Button, Tooltip } from "@mui/material";
+import React from "react";
+
+export type POSDialogHeaderProps = {
+  titleDialog: string;
+  setIsOpenDialog: Function;
+};
+
+const POSDialogHeader: React.FC<POSDialogHeaderProps> = ({ titleDialog, setIsOpenDialog }) => {
+  return (
+    <AppBar sx={{ position: "relative" }}>
+      <Toolbar sx={{ display: "flex" }}>
+        <div style={{ flex: "50%" }}>
+          <Typography sx={{ ml: 2, flex: 1, color: "white" }} variant="h6" component="div">
+            {titleDialog}
+          </Typography>
+        </div>
+        <div style={{ flex: "50%", textAlign: "end" }}>
+          <Tooltip title="Cerrar">
+            <IconButton aria-label="logOut" onClick={() => setIsOpenDialog(false)}>
+              <CloseIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default POSDialogHeader;
