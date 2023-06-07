0f5504e2f353aca23e1264951f8a52afb551a779
Reign-Beaux <samc920130@hotmail.com>
2023-06-07 11:36:52 -0600

    se reacomodaron lineas

--- src/pages/Purchases/components/PurchasingProcess/components/PurchasingProcessDetail/components/PPDetailDataGrid/PPDetailDataGrid.tsx
+++ src/pages/Purchases/components/PurchasingProcess/components/PurchasingProcessDetail/components/PPDetailDataGrid/PPDetailDataGrid.tsx
@@ -1,10 +1,7 @@
-import { calculateValues } from "common/utilities";
-import { SelectChangeEvent } from "@mui/material";
 import { POSDataGrid } from "common/components";
 import { useAxios, useDialogConfirm } from "common/custom-hooks";
 import { PurchaseDTO } from "common/dtos";
-import { PurchaseDetail } from "common/models";
-import React, { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 import { usePPDetailContext } from "../../context";
 import { useColumnsPPDetail } from "./custom-hooks";
 
@@ -14,9 +11,9 @@ const PPDetailDataGrid: React.FC<PPDetailDataGridProps> = () => {
   const { purchaseDetail } = usePPDetailContext();
   const { getAll, remove } = useAxios("Purchases");
   const { showDialogConfirm, resetResponse, response } = useDialogConfirm();
-  const [Purchases, setPurchases] = useState<PurchaseDTO[]>([]);
-
   const { columns } = useColumnsPPDetail();
+  
+  const [Purchases, setPurchases] = useState<PurchaseDTO[]>([]);
 
   return <POSDataGrid dataSource={purchaseDetail} columns={columns()} />;
 };
