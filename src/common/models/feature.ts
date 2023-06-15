bc1d2127bd7aa552f802b0e336e3ac7e34b21c86
Reign-Beaux <samc920130@hotmail.com>
2023-06-15 14:48:46 -0600

    se agregó la propiedad ischildren a feature

--- src/common/models/feature.ts
+++ src/common/models/feature.ts
@@ -2,9 +2,11 @@ import { POSCatalogue, posCatalogueEmpty } from "./posCatalogue";
 
 export interface Feature extends POSCatalogue {
   direction: string;
+  isChildren: boolean;
 }
 
 export const featureEmpty: Feature = {
   ...posCatalogueEmpty,
-  direction: ""
+  direction: "",
+  isChildren: false,
 }
