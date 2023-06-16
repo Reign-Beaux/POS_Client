c046981611ff11bf9e91636fd7995a139d992a51
Reign-Beaux <samc920130@hotmail.com>
2023-06-16 16:53:34 -0600

    Se documentaron las constantes y los regex

--- src/common/consts/POSConsts.ts
+++ src/common/consts/POSConsts.ts
@@ -1,22 +1,39 @@
+/**
+ * Tipos de roles de usuario.
+ */
 export type UserRoles = "admin" | "sales" | "purchases" | "inventory";
 
+/**
+ * Colores de alerta.
+ */
 export enum AlertColors {
   ERROR = "error",
   WARNING = "warning",
   INFO = "info",
   SUCCESS = "success",
 }
-
+/**
+ * Ancho del drawer (cajón lateral).
+ */
 export const DRAWER_WIDTH = 240;
 
+/**
+ * Mensajes de snack (notificación breve).
+ */
 export enum SnackMessage {
   SUCCESS = "Operación realizada correctamente",
 }
 
+/**
+ * Mensajes de confirmación para acciones.
+ */
 export enum ConfirmActionMessage {
   DELETE = "¿Desea eliminar el registro seleccionado?",
 }
 
+/**
+ * Tasas de impuestos.
+ */
 export enum Taxes {
   IVA = 0.16,
 }
--- src/common/consts/POSRegex.ts
+++ src/common/consts/POSRegex.ts
@@ -1,5 +1,9 @@
-//Valida correo electrónico
+/**
+ * Valida correo electrónico
+*/
 export const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-//Valida el uso unicamente del sistema numérico con decimales
+/**
+ * Valida el uso unicamente del sistema numérico con decimales
+*/
 export const numericRegex = /^[0-9]+(?:\.[0-9]*)?$/;
